c526c2cc8d9ad25c9e5cfe8483303bbe4744248b
tcaironglnan <995956062@qq.com>
2018-12-04 17:26:27 +0800

    1.add 临时提交

---
+++ src/main/java/com/ssm/controller/QuartzJobTask.java
@@ -0,0 +1,8 @@
+package com.ssm.controller;
+
+/**
+ * @author Lenovo
+ * @Created 2018-12-04 17:11
+ **/
+public class QuartzJobTask {
+}
--- src/main/java/com/ssm/service/impl/QuartzServiceImpl.java
+++ src/main/java/com/ssm/service/impl/QuartzServiceImpl.java
@@ -12,28 +12,21 @@ public class QuartzServiceImpl implements QuartzService {
     private Scheduler quartzScheduler;
 
     @Override
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public void addJob(String jobName, String jobGroupName, String triggerName, String triggerGroupName, Class clazz, String cron) {
+    public void addJob(String jobName, String jobGroupName, String triggerName, String triggerGroupName, Class cls, String cron) {
         try {
-            //获取调度器
-            SchedulerFactory schedulerFactory = (SchedulerFactory)quartzScheduler;
-            Scheduler scheduler = schedulerFactory.getScheduler();
-            // 任务名，任务组，任务执行类
-            JobDetail jobDetail = JobBuilder.newJob(clazz).withIdentity(jobName, jobGroupName).build();
-            // 触发器
-            TriggerBuilder<Trigger> triggerBuilder = TriggerBuilder.newTrigger();
-            // 触发器名,触发器组
-            triggerBuilder.withIdentity(triggerName, triggerGroupName);
-            triggerBuilder.startNow();
-            // 触发器时间设定
-            triggerBuilder.withSchedule(CronScheduleBuilder.cronSchedule(cron));
-            // 创建Trigger对象
-            CronTrigger trigger = (CronTrigger) triggerBuilder.build();
-            // 调度容器设置JobDetail和Trigger
-            scheduler.scheduleJob(jobDetail, trigger);
+            // 获取调度器
+            // 创建一项作业
+            JobDetail job = JobBuilder.newJob(cls).withIdentity(jobName, jobGroupName).build();
+            // 创建一个触发器
+            CronTrigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity(triggerName, triggerGroupName)
+                    .withSchedule(CronScheduleBuilder.cronSchedule(cron))
+                    .build();
+            // 告诉调度器使用该触发器来安排作业
+            quartzScheduler.scheduleJob(job, trigger);
             // 启动
-            if (!scheduler.isShutdown()) {
-                scheduler.start();
+            if (!quartzScheduler.isShutdown()) {
+                quartzScheduler.start();
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -44,32 +37,25 @@ public class QuartzServiceImpl implements QuartzService {
      * 修改定时器任务信息
      */
     @Override
-    public boolean modifyJobTime(String jobName, String jobGroupName, String triggerName, String triggerGroupName, String cron) {
+    public boolean modifyJobTime(String jobName, String jobGroup, String triggerName, String triggerGroup, String cron) {
         try {
-            //获取调度器
-            SchedulerFactory schedulerFactory = (SchedulerFactory)quartzScheduler;
-            Scheduler scheduler = schedulerFactory.getScheduler();
-            //初始化触发器的触发器参数
-            TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroupName);
-            //创建触发器
-            CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
-            //判断触发器是否有创建
+            CronTrigger trigger = (CronTrigger) quartzScheduler.getTrigger(TriggerKey.triggerKey(triggerName, triggerGroup));
             if (trigger == null) {
                 return false;
             }
-            //获取上一次的定时时间表达式
-            String oldTime = trigger.getCronExpression();
-            //判断两次的时间是否一样
-            if (!oldTime.equalsIgnoreCase(cron)) {
-                //获取上一次的定义定时任务实例
-                JobDetail jobDetail = scheduler.getJobDetail(JobKey.jobKey(jobName, jobGroupName));
-                //获取业务执行类
-                Class<? extends Job> jobClass = jobDetail.getJobClass();
-                //调用移除定时器方法
-                removeJob(jobName, jobGroupName, triggerName, triggerGroupName);
-                //调用添加定时器方法
-                addJob(jobName, jobGroupName, triggerName, triggerGroupName, jobClass, cron);
-            }
+
+            JobKey jobKey = JobKey.jobKey(jobName, jobGroup);
+            TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroup);
+
+            JobDetail job = quartzScheduler.getJobDetail(jobKey);
+            Class jobClass = job.getJobClass();
+            // 停止触发器
+            quartzScheduler.pauseTrigger(triggerKey);
+            // 移除触发器
+            quartzScheduler.unscheduleJob(triggerKey);
+            // 删除任务
+            quartzScheduler.deleteJob(jobKey);
+            addJob(jobName, jobGroup, triggerName, triggerGroup, jobClass, cron);
             return true;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -80,33 +66,16 @@ public class QuartzServiceImpl implements QuartzService {
     @Override
     public void modifyJobTime(String triggerName, String triggerGroupName, String cron) {
         try {
-            //获取调度器
-            SchedulerFactory schedulerFactory = (SchedulerFactory)quartzScheduler;
-            Scheduler scheduler = schedulerFactory.getScheduler();
-            //初始化触发器的触发器参数
-            TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroupName);
-            //创建触发器
-            CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
-            //判断触发器是否有创建
+            CronTrigger trigger = (CronTrigger) quartzScheduler.getTrigger(TriggerKey.triggerKey(triggerName, triggerGroupName));
             if (trigger == null) {
                 return;
             }
-            //获取上一次的定时时间表达式
             String oldTime = trigger.getCronExpression();
-            //如果更新的时间一致的话，则不进行修改操作
             if (!oldTime.equalsIgnoreCase(cron)) {
-                // 触发器 创建新的触发器
-                TriggerBuilder<Trigger> triggerBuilder = TriggerBuilder.newTrigger();
-                // 触发器名,触发器组
-                triggerBuilder.withIdentity(triggerName, triggerGroupName);
-                //开始执行触发器
-                triggerBuilder.startNow();
-                // 触发器时间设定
-                triggerBuilder.withSchedule(CronScheduleBuilder.cronSchedule(cron));
-                // 创建Trigger对象
-                trigger = (CronTrigger) triggerBuilder.build();
-                //修改一个任务的触发时间 即：用新的触发器替换旧的触发器
-                scheduler.rescheduleJob(triggerKey, trigger);
+                // 修改时间
+                trigger.getTriggerBuilder().withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
+                // 重启触发器
+                quartzScheduler.resumeTrigger(TriggerKey.triggerKey(triggerName, triggerGroupName));
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
