d888f976550c652201aaf2b7ccce3c2855e1cb77
tcaironglnan <995956062@qq.com>
2018-12-05 14:34:22 +0800

    1.add 临时提交

--- src/main/java/com/ssm/service/impl/QuartzServiceImpl.java
+++ src/main/java/com/ssm/service/impl/QuartzServiceImpl.java
@@ -7,6 +7,64 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * 定时器实现类
+ * 注：
+ * quartz在动态创建和修改触发器信息时，会触发立刻执行
+ * 按如下方法
+ * CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getJobExpression()).withMisfireHandlingInstructionDoNothing();
+ * withMisfireHandlingInstructionDoNothing
+ * ——不触发立即执行
+ * ——等待下次Cron触发频率到达时刻开始按照Cron频率依次执行
+ *
+ * CronTrigger
+ * withMisfireHandlingInstructionDoNothing
+ * ——不触发立即执行
+ * ——等待下次Cron触发频率到达时刻开始按照Cron频率依次执行
+ * withMisfireHandlingInstructionIgnoreMisfires
+ * ——以错过的第一个频率时间立刻开始执行
+ * ——重做错过的所有频率周期后
+ * ——当下一次触发频率发生时间大于当前时间后，再按照正常的Cron频率依次执行
+ * withMisfireHandlingInstructionFireAndProceed
+ * ——以当前时间为触发频率立刻触发一次执行
+ * ——然后按照Cron频率依次执行
+ * SimpleTrigger
+ * withMisfireHandlingInstructionFireNow
+ * ——以当前时间为触发频率立即触发执行
+ * ——执行至FinalTIme的剩余周期次数
+ * ——以调度或恢复调度的时刻为基准的周期频率，FinalTime根据剩余次数和当前时间计算得到
+ * ——调整后的FinalTime会略大于根据starttime计算的到的FinalTime值
+ * withMisfireHandlingInstructionIgnoreMisfires
+ * ——以错过的第一个频率时间立刻开始执行
+ * ——重做错过的所有频率周期
+ * ——当下一次触发频率发生时间大于当前时间以后，按照Interval的依次执行剩下的频率
+ * ——共执行RepeatCount+1次
+ * withMisfireHandlingInstructionNextWithExistingCount
+ * ——不触发立即执行
+ * ——等待下次触发频率周期时刻，执行至FinalTime的剩余周期次数
+ * ——以startTime为基准计算周期频率，并得到FinalTime
+ * ——即使中间出现pause，resume以后保持FinalTime时间不变
+ *
+ * withMisfireHandlingInstructionNowWithExistingCount
+ * ——以当前时间为触发频率立即触发执行
+ * ——执行至FinalTIme的剩余周期次数
+ * ——以调度或恢复调度的时刻为基准的周期频率，FinalTime根据剩余次数和当前时间计算得到
+ * ——调整后的FinalTime会略大于根据starttime计算的到的FinalTime值
+ * withMisfireHandlingInstructionNextWithRemainingCount
+ * ——不触发立即执行
+ * ——等待下次触发频率周期时刻，执行至FinalTime的剩余周期次数
+ * ——以startTime为基准计算周期频率，并得到FinalTime
+ * ——即使中间出现pause，resume以后保持FinalTime时间不变
+ * withMisfireHandlingInstructionNowWithRemainingCount
+ * ——以当前时间为触发频率立即触发执行
+ * ——执行至FinalTIme的剩余周期次数
+ * ——以调度或恢复调度的时刻为基准的周期频率，FinalTime根据剩余次数和当前时间计算得到
+ * ——调整后的FinalTime会略大于根据starttime计算的到的FinalTime值
+ * MISFIRE_INSTRUCTION_RESCHEDULE_NOW_WITH_REMAINING_REPEAT_COUNT
+ * ——此指令导致trigger忘记原始设置的starttime和repeat-count
+ * ——触发器的repeat-count将被设置为剩余的次数
+ * ——这样会导致后面无法获得原始设定的starttime和repeat-count值
+ */
 @Service("quartzService")
 public class QuartzServiceImpl implements QuartzService {
 
